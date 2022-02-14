42e482cdb77891370c79deb00f6fa2fdd3e194a2
SirTypebrey <bgonzalezp02@educantabria.es>
2022-02-14 05:55:28 +0100

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import { MenuElements } from './data/MenuElements';
-import Barrita from './components/Barrita';
+import Barrita from './data/Barrita';
 
 export default function App() {
   return (
     <Router>
       <Barrita />
-      <h1>Desarrollo de Interfaces: Examen React.js </h1>
-      <p>
-        Resuelve los ejercicios propuestos en los distintos apartados de la
-        barra de navegación. Para ello, haz un fork de este repositorio desde tu
-        cuenta de Github y súbelo a la tarea de examen. El repositorio debe
-        tener el nombre examen-react-Nombre-Apellido1, donde nombre y apellido
-        son los tuyos.
-      </p>
       {MenuElements.map((item) => {
         return (
           <Route
